4caa3d7b5550342915c79049d57f5f973b599e00
Fernando Salgado <fernando.salgado@modalgr.com.br>
2019-05-09 16:48:40 -0300

    Cursos Alura

---
+++ Curso C# - Array e Tipos Genericos/csharppt7-aula0/ByteBank.SistemaAgencia/ListaDeObjects.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class ListaDeObjects
+    {
+        private object[] _items;
+        private int _proximaPosicao;
+        public int Tamanho
+        {
+            get
+            {
+                return _proximaPosicao;
+            }
+        }
+
+        public ListaDeObjects()
+        {
+            _items = new object[5];
+            _proximaPosicao = 0;
+        }
+        public void Adicionar(object conta)
+        {
+            VerificarCapacidade(_proximaPosicao + 1);
+            //Console.WriteLine($"Adicionando item na posição {_proximaPosicao}");
+            _items[_proximaPosicao] = conta;
+            _proximaPosicao++;
+        }
+
+        public void AdicionarVarios(params object[] contas)
+        {
+            foreach (object conta in contas)
+            {
+                Adicionar(conta);
+            }
+        }
+
+        public void Remover(object item)
+        {
+            int indiceItem = -1;
+            for (int i = 0; i < _proximaPosicao; i++)
+            {
+                object itemAtual = _items[i];
+
+                if (itemAtual.Equals(item))
+                {
+                    indiceItem = i;
+                    break;
+                }
+            }
+            for (int i = indiceItem; i < _proximaPosicao - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+            _proximaPosicao--;
+            _items[_proximaPosicao] = null;
+        }
+        public void EscreverListaNaTela()
+        {
+            for (int i = 0; i < _proximaPosicao; i++)
+            {
+                object item = _items[i];
+                //Console.WriteLine($"Conta: {conta.Agencia}{conta.Numero}");
+            }
+        }
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if (_items.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+            int novoTamanho = _items.Length * 2;
+            if (novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+            //Console.WriteLine("Aumentando lista.");
+            object[] novoArray = new object[novoTamanho];
+
+            for (int i = 0; i < _items.Length; i++)
+            {
+                novoArray[i] = _items[i];
+            }
+
+            _items = novoArray;
+        }
+
+        public object GetItemNoIndice(int i)
+        {
+            if (i < 0 || i >= _proximaPosicao)
+            {
+                throw new ArgumentOutOfRangeException(nameof(i));
+            }
+            return _items[i];
+        }
+
+        public object this[int i]
+        {
+            get
+            {
+                return GetItemNoIndice(i);
+            }
+        }
+    }
+}
---
+++ Curso C# - Array e Tipos Genericos/csharppt7-aula0/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using ByteBank.SistemaAgencia.Comparadores;
+using ByteBank.SistemaAgencia.Extensoes;
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contas = new List<ContaCorrente>
+            {
+                new ContaCorrente(007,333),
+                new ContaCorrente(003,111),
+                new ContaCorrente(002,777),
+                new ContaCorrente(001,222),
+                new ContaCorrente(000,555),
+            };
+            //contas.Sort();
+            contas.Sort(new ComparadorContaCorrentePorAgencia());
+            foreach (var item in contas)
+            {
+                Console.WriteLine($"Conta numero {item.Numero}, ag. {item.Agencia}");
+            }
+            Console.ReadLine();
+        }
+
+        static void TestaLista()
+        {
+            //ListaDeContaCorrente lista = new ListaDeContaCorrente();
+            ListaDeObjects lista = new ListaDeObjects();
+            ContaCorrente conta = new ContaCorrente(111, 1111);
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                conta,
+                new ContaCorrente(874,800),
+                new ContaCorrente(874,801),
+                new ContaCorrente(874,802),
+                new ContaCorrente(874,803),
+                new ContaCorrente(874,804),
+                new ContaCorrente(874,805),
+                new ContaCorrente(874,806)
+            };
+            lista.AdicionarVarios(contas);
+            lista.AdicionarVarios(new ContaCorrente(874, 807), new ContaCorrente(874, 808), new ContaCorrente(874, 809));
+
+            for (int i = 0; i < lista.Tamanho; i++)
+            {
+                ContaCorrente itemAtual = lista[i] as ContaCorrente;
+                Console.WriteLine($"Item na posição {i}: Conta {itemAtual.Numero}/{itemAtual.Agencia}.");
+            }
+        }
+        static void TestaArrayInt()
+        {
+            int[] idade = new int[5];
+            idade[0] = 15;
+            idade[1] = 22;
+            idade[2] = 33;
+            idade[3] = 44;
+            idade[4] = 55;
+
+            int acumulador = 0;
+            for (int i = 0; i < idade.Length; i++)
+            {
+                Console.WriteLine($"Acessando o array Idade no índice: {i}.");
+                Console.WriteLine($"Valor de idade[{i}]={idade[i]}");
+                acumulador += idade[i];
+            }
+            int media = acumulador / idade.Length;
+            Console.WriteLine($"A media de idades dividido por {idade.Length} é: {media}");
+
+        }
+    }
+}
