8b0d440d8c1796d3d35dfb94fefa3ba9af35ba5c
Fernando Salgado <fernando.salgado@modalgr.com.br>
2019-05-22 12:17:47 -0300

    Finalização curso C# Collections

---
+++ Curso Alura - Array/LacoForeach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacoForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayMeses = new string[]
+            {
+                "Janeiro", "Fevereiro", "Março",
+                "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro"
+            };
+            foreach (var item in arrayMeses)
+            {
+                arrayMeses[1] = "A";
+                Console.WriteLine(item);
+            }
+            IList<string> meses = new List<string>
+            {
+                "Janeiro", "Fevereiro", "Março",
+                "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro"
+            };
+            foreach (var item in meses)
+            {
+                //meses[0] = "A";
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Curso Alura - Array/OperadoresDeConjuntos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresDeConjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] seq1 = { "Janeiro","Fevereiro","Março"};
+            string[] seq2 = { "Fevereiro","MARÇO","Abril"};
+            var consulta = seq1.Concat(seq2);
+            Console.WriteLine("Concat:");
+            ExibeConsulta(consulta);
+            consulta = seq1.Union(seq2,StringComparer.CurrentCultureIgnoreCase);
+            Console.WriteLine("Union:");
+            ExibeConsulta(consulta);
+            consulta = seq1.Intersect(seq2);
+            Console.WriteLine("Intersect:");
+            ExibeConsulta(consulta);
+            consulta = seq1.Except(seq2);
+            Console.WriteLine("Except:");
+            ExibeConsulta(consulta);
+        }
+
+        static void ExibeConsulta(dynamic consulta)
+        {
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
